3f1b4877e3f2130e25fe3d9b96bc7c0afdf90e49
dmcmillon-novosfiber <160290440+dmcmillon-novosfiber@users.noreply.github.com>
2024-09-27 15:32:32 -0500

    Add files via upload
    You need to create a CSV file that has the headings and the example below 
    
    userPrincipalName	First Name	Last Name	Job Title	Company Name	Employee Hire Date	Office Location	Manager Name	ManagerUserPrincipalName
    user.1@primefiberco.com	User	1 Community Liaison Manager	PRIME FiBER	7/22/2024	4851 Tamiami Trail N, Ste 200 Naples, FL 34103	User 2	user.2@primefiberco.com

---
+++ Entra_Employee_Update.ps1
@@ -0,0 +1,34 @@
+﻿# Connect to Microsoft Graph
+Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+# Import CSV file with user data
+$users = Import-Csv -Path "C:\Users\DarrellMcMillon\OneDrive - NOVOS FiBER\Entra_HR_Updates.csv"
+
+foreach ($user in $users) {
+    $upn = $user.UserPrincipalName
+    $jobTitle = $user.'Job Title'
+    $hireDate = $user.'Employee Hire Date'
+    $managerUpn = $user.ManagerUserPrincipalName
+    $companyName = $user.'Company Name'
+    $officeLocation = $user.'Office Location'
+
+    # Get the user object
+    $mgUser = Get-MgUser -UserId $upn
+
+    # Get the manager object
+    $manager = Get-MgUser -UserId $managerUpn
+
+    # Manager Ref
+    $ManagerRef = @{"@odata.id" = "https://graph.microsoft.com/v1.0/users/$managerUpn"}
+
+    # Update user properties
+    Update-MgUser -UserId $mgUser.Id -JobTitle $jobTitle -EmployeeHireDate $hireDate -CompanyName $companyName -OfficeLocation $officeLocation
+
+    # Set the manager
+    Set-MgUserManagerbyRef -UserId $mgUser.Id -BodyParameter $ManagerRef
+
+    Write-Host "Updated user: $upn"
+}
+
+# Disconnect from Microsoft Graph
+Disconnect-MgGraph
